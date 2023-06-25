645ddfbae273f3d913253c7c1c8f1d912a7dd552
Aisha Hussein <aishasalaam07@gmail.com>
2023-06-24 22:21:23 -0400

    added migrated files

--- db/migrate/20230623221002_create_pizzas.rb
+++ db/migrate/20230623221002_create_pizzas.rb
@@ -1,10 +1,10 @@
-class CreatePizzas < ActiveRecord::Migration[7.0]
-  def change
-    create_table :pizzas do |t|
-      t.string :name
-      t.string :ingredients
+# class CreatePizzas < ActiveRecord::Migration[7.0]
+#   def change
+#     create_table :pizzas do |t|
+#       t.string :name
+#       t.string :ingredients
 
-      t.timestamps
-    end
-  end
-end
+#       t.timestamps
+#     end
+#   end
+# end
