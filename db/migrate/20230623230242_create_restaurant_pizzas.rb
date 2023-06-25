8753a178527c6439d84c813416d811c1c8d51ddc
Aisha Hussein <aishasalaam07@gmail.com>
2023-06-24 22:21:56 -0400

    updated

--- db/migrate/20230623230242_create_restaurant_pizzas.rb
+++ db/migrate/20230623230242_create_restaurant_pizzas.rb
@@ -1,8 +1,8 @@
-# class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
-#   def change
-#     create_table :restaurant_pizzas do |t|
+class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :restaurant_pizzas do |t|
 
-#       t.timestamps
-#     end
-#   end
-# end
+      t.timestamps
+    end
+  end
+end
